3e09253d4840186dff6650a464c94761741ee3b2
Matute04 <ezequielcoronado48@gmail.com>
2023-05-10 21:15:29 -0400

    first commit

---
+++ Intermediate/Build/Win64/UE4Editor/Inc/TetrisUSFX01/PiezaHielo.generated.h
@@ -0,0 +1,98 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef TETRISUSFX01_PiezaHielo_generated_h
+#error "PiezaHielo.generated.h already included, missing '#pragma once' in PiezaHielo.h"
+#endif
+#define TETRISUSFX01_PiezaHielo_generated_h
+
+#define TetrisUSFX_PARCIAL_master_Source_TetrisUSFX01_PiezaHielo_h_13_SPARSE_DATA
+#define TetrisUSFX_PARCIAL_master_Source_TetrisUSFX01_PiezaHielo_h_13_RPC_WRAPPERS
+#define TetrisUSFX_PARCIAL_master_Source_TetrisUSFX01_PiezaHielo_h_13_RPC_WRAPPERS_NO_PURE_DECLS
+#define TetrisUSFX_PARCIAL_master_Source_TetrisUSFX01_PiezaHielo_h_13_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAPiezaHielo(); \
+	friend struct Z_Construct_UClass_APiezaHielo_Statics; \
+public: \
+	DECLARE_CLASS(APiezaHielo, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/TetrisUSFX01"), NO_API) \
+	DECLARE_SERIALIZER(APiezaHielo) \
+	virtual UObject* _getUObject() const override { return const_cast<APiezaHielo*>(this); }
+
+
+#define TetrisUSFX_PARCIAL_master_Source_TetrisUSFX01_PiezaHielo_h_13_INCLASS \
+private: \
+	static void StaticRegisterNativesAPiezaHielo(); \
+	friend struct Z_Construct_UClass_APiezaHielo_Statics; \
+public: \
+	DECLARE_CLASS(APiezaHielo, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/TetrisUSFX01"), NO_API) \
+	DECLARE_SERIALIZER(APiezaHielo) \
+	virtual UObject* _getUObject() const override { return const_cast<APiezaHielo*>(this); }
+
+
+#define TetrisUSFX_PARCIAL_master_Source_TetrisUSFX01_PiezaHielo_h_13_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API APiezaHielo(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(APiezaHielo) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, APiezaHielo); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(APiezaHielo); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API APiezaHielo(APiezaHielo&&); \
+	NO_API APiezaHielo(const APiezaHielo&); \
+public:
+
+
+#define TetrisUSFX_PARCIAL_master_Source_TetrisUSFX01_PiezaHielo_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API APiezaHielo(APiezaHielo&&); \
+	NO_API APiezaHielo(const APiezaHielo&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, APiezaHielo); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(APiezaHielo); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(APiezaHielo)
+
+
+#define TetrisUSFX_PARCIAL_master_Source_TetrisUSFX01_PiezaHielo_h_13_PRIVATE_PROPERTY_OFFSET \
+	FORCEINLINE static uint32 __PPO__Pieza() { return STRUCT_OFFSET(APiezaHielo, Pieza); }
+
+
+#define TetrisUSFX_PARCIAL_master_Source_TetrisUSFX01_PiezaHielo_h_10_PROLOG
+#define TetrisUSFX_PARCIAL_master_Source_TetrisUSFX01_PiezaHielo_h_13_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TetrisUSFX_PARCIAL_master_Source_TetrisUSFX01_PiezaHielo_h_13_PRIVATE_PROPERTY_OFFSET \
+	TetrisUSFX_PARCIAL_master_Source_TetrisUSFX01_PiezaHielo_h_13_SPARSE_DATA \
+	TetrisUSFX_PARCIAL_master_Source_TetrisUSFX01_PiezaHielo_h_13_RPC_WRAPPERS \
+	TetrisUSFX_PARCIAL_master_Source_TetrisUSFX01_PiezaHielo_h_13_INCLASS \
+	TetrisUSFX_PARCIAL_master_Source_TetrisUSFX01_PiezaHielo_h_13_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define TetrisUSFX_PARCIAL_master_Source_TetrisUSFX01_PiezaHielo_h_13_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TetrisUSFX_PARCIAL_master_Source_TetrisUSFX01_PiezaHielo_h_13_PRIVATE_PROPERTY_OFFSET \
+	TetrisUSFX_PARCIAL_master_Source_TetrisUSFX01_PiezaHielo_h_13_SPARSE_DATA \
+	TetrisUSFX_PARCIAL_master_Source_TetrisUSFX01_PiezaHielo_h_13_RPC_WRAPPERS_NO_PURE_DECLS \
+	TetrisUSFX_PARCIAL_master_Source_TetrisUSFX01_PiezaHielo_h_13_INCLASS_NO_PURE_DECLS \
+	TetrisUSFX_PARCIAL_master_Source_TetrisUSFX01_PiezaHielo_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> TETRISUSFX01_API UClass* StaticClass<class APiezaHielo>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID TetrisUSFX_PARCIAL_master_Source_TetrisUSFX01_PiezaHielo_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
